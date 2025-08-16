c17ed92925da02b3f7b3b8fc9266774bcd0f4dad
joshjones49 <joshjones200285@gmail.com>
2025-08-16 14:00:27 -0400

    Created Card entity

---
+++ javacheck/src/test/java/com/joshjones49/javacheck/JavacheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshjones49.javacheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavacheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
