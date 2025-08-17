c864d30470a1dce31d7b19f56d25a1db74047ca7
joshjones49 <joshjones200285@gmail.com>
2025-08-17 16:49:58 -0400

    Added React client

--- javacheck/src/main/java/com/joshjones49/javacheck/repos/CardRepo.java
+++
@@ -1,9 +0,0 @@
-package com.joshjones49.javacheck.repos;
-
-import com.joshjones49.javacheck.entities.Card;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CardRepo extends JpaRepository<Card, Long> {
-}
