37dee0f151cde81d3765533562d3826c63e3c391
joshjones49 <joshjones200285@gmail.com>
2025-08-18 22:40:04 -0400

    Working on Review page

--- client-javacheck/src/pages/Review/Review.jsx
+++ client-javacheck/src/pages/Review/Review.jsx
@@ -1,9 +1,33 @@
 import './Review.css'
 
+import { Link } from 'react-router-dom'
+
 const Review = () => {
   return (
     <div className='review pages' >
-      REVIEW
+
+      <div className='review-menu' >
+
+        <input type="text" placeholder='search' />
+
+        <div className='category-ctn' >
+          <Link className='r-links'>
+            <h4>OOP</h4>
+          </Link>
+          <Link className='r-links'>
+            <h4>Spring-Boot</h4>
+          </Link>
+          <Link className='r-links'>
+            <h4>Maven</h4>
+          </Link>
+        </div>
+
+      </div>
+
+      <div className='review-right' >
+        RIGHT
+      </div>
+
     </div>
   )
 }
